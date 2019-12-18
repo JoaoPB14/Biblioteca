e52147a2a4a0f43af147b325b887a7ed2c0d2283
JoaoPB14 <56631964+JoaoPB14@users.noreply.github.com>
2019-12-18 05:40:37 -0300

    Add files via upload

---
+++ Livro.java
@@ -0,0 +1,69 @@
+import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Livro extends Autor {
+
+	private String titulo;
+	private String editora;
+	private String dataPublic;
+	private String autor;
+	Scanner s = new Scanner(System.in);
+	
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	
+	public void setEditora(String editora) {
+		this.editora = editora;
+	}
+	
+	public void setDataPublic(String dataPublic) {
+		this.dataPublic = dataPublic;
+	}
+	
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	
+	public String getTitulo() {
+		return this.titulo;
+	}
+	
+	public String getEditora() {
+		return this.editora;
+	}
+	
+	public String getDataPublic() {
+		return this.dataPublic;
+	}
+	
+	public String getAutor() {
+		return this.autor;
+	}
+
+	public void addLivro() {
+		
+		Livro livro;
+		livro = new Livro();
+		System.out.print("Digite o Titulo: ");
+	    livro.setTitulo(s.nextLine());
+	    System.out.print("Digite a Editora: ");
+	    livro.setEditora(s.nextLine());
+	    System.out.print("Digite a Data da publicação: ");
+	    livro.setDataPublic(s.nextLine());
+	    System.out.print("Digite o nome do(s) Autor(es): ");
+	    livro.setAutor(s.nextLine());
+	   
+	    ArrayList<String> livros = new ArrayList<String>();
+	    
+	    livros.add(getTitulo());
+	    livros.add(getEditora());
+	    livros.add(getDataPublic());
+	    livros.add(getAutor());
+	    
+	}
+}
+
+
+
